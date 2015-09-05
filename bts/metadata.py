0042ca8569c5870354222934a2e341a878be8ccf
alt <pch957@163.com>
2015-09-05 22:45:12 +0800

    new version

--- bts/metadata.py
+++ bts/metadata.py
@@ -8,7 +8,7 @@ Information describing the project.
 package = 'bts'
 project = "python api for BitShares"
 project_no_spaces = project.replace(' ', '')
-version = '0.5.15'
+version = '0.5.16'
 description = 'api for BitShares'
 authors = ['Alt']
 authors_string = ', '.join(authors)
